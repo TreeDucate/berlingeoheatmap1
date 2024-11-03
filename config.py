7b8085b662d032f8c6192218c1a1afac1d0e5e73
MathforDataScience <42959174+MathforDataScience@users.noreply.github.com>
2024-11-03 21:05:25 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ p['picklefolder']           = 'pickles'
 p['geocode']                = 'PLZ'
 
 p["file_lstations"]         = "Ladesaeulenregister.csv"
-p["file_buildings"]         = "gebaeude.csv"
+# p["file_buildings"]         = "gebaeude.csv"
 p["file_residents"]         = "plz_einwohner.csv"
-p["file_amounttraf"]        = "Verkehrsaufkommen.csv"
+# p["file_amounttraf"]        = "Verkehrsaufkommen.csv"
 
 p["file_geodat_plz"]       = "geodata_berlin_plz.csv"
 p["file_geodat_dis"]       = "geodata_berlin_dis.csv"
 
-p["gebaeude_filter"]        = ["Freistehendes Einzelgebäude", "Doppelhaushälfte"]
+# p["gebaeude_filter"]        = ["Freistehendes Einzelgebäude", "Doppelhaushälfte"]
 
 # -----------------------------------
 pdict = p.copy()
